2a41a89700e3ce87c2b0dcb5f0c78ea557bb66bd
ppegotsu <ppengotsu@sysken.tokyo>
2023-08-18 01:52:15 +0900

    エラーでるように

--- app/common/commonrequireall.php
+++ app/common/commonrequireall.php
@@ -1,4 +1,7 @@
 <?php
+ini_set( 'display_errors', 1 );
+
+
 require_once(dirname(__FILE__).'/Commondefine.php');
 require_once(dirname(__FILE__).'/AppDBManager.php');
 require_once(dirname(__FILE__).'/AppInfoJSON.php');
